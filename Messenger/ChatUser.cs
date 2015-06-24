8de85d3461ba21749167d02d695f15f677a34f40
Martynas Kan <martis347@gmail.com>
2015-06-24 18:14:28 +0300

    Initial commit 2

---
+++ Messenger/ChatUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Messenger.Displays;
+
+namespace Messenger
+{
+    public class ChatUser
+    {
+        public string Username { get; private set; }
+
+        public string CurrentChatRoomName { get; set; }
+
+        public UserDisplay Display { get; private set; }
+
+        private readonly IDisplay _display;
+
+        public ChatUser(string username, IDisplay display)
+        {
+            if (username == null) 
+                throw new Exception("Username is null");
+            Username = username;
+
+            _display = display;
+        }
+    }
+}
