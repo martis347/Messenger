3e513737056bb115a6b73935c1328a10ab25de04
Martynas Kan <martis347@gmail.com>
2015-06-25 23:21:56 +0300

    Added ChatInfo enum

---
+++ Messenger/ChatInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace Messenger
+{
+    public class ChatInfo
+    {
+        public string NewMessages { get; set; }
+        public int UsersInRoom { get; set; }
+        public HttpStatusCode Status { get; set; }
+    }
+}
