543dfc808d67fd9d542e278660a4956fe0f28c75
Martynas Kanapinskas <martis347@gmail.com>
2015-07-02 21:15:25 +0300

    Removed all projects to src catalog

--- Messenger/ChatInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Messenger
-{
-    public class ChatInfo
-    {
-        public string NewMessages { get; set; }
-        public int UsersInRoom { get; set; }
-        public RequestStatus Status { get; set; }
-    }
-}
